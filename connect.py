948fca7f8e593e328c4b80f437a877abe3a38b7a
AustinATracy <167483725+AustinATracy@users.noreply.github.com>
2024-04-23 13:00:14 -0700

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import udf, col
+from snowflake.snowpark.types import Variant
+from snowflake.snowpark import functions as fn
+
+import pandas as pd
+import numpy as np
 
 from snowflake.snowpark import version
-from snowflake.snowpark.session import Session
 
 #horray
 
