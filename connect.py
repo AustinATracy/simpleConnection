32361ac8961849a5c9b2918d93b94c6b77d8c6d5
AustinATracy <167483725+AustinATracy@users.noreply.github.com>
2024-04-23 13:29:22 -0600

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from snowflake.snowpark import version
+from snowflake.snowpark.session import Session
+
+#horray
+
+st.write(version.VERSION)
+
+
+credentials = {
+  "account": "AITNLHM-TYB61838",
+  "user": "AustinTracy7",
+  "password": st.secrets["password"],
+  "role": "ACCOUNTADMIN",
+  "database": "IMDB",
+  "schema": "PUBLIC",
+  "warehouse": "COMPUTE_WH"
+}
+
+
+st.write(credentials)
+
+session = Session.builder.configs(credentials).create()
+
+st.write(session)
